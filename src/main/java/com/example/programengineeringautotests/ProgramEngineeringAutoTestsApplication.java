eb0e96926374dcde5c7f4947b59e38e5069e4ac3
Oybolatov Ramazan <ramazanoybolatov@gmail.com>
2024-05-25 05:32:56 +0300

    Понизил версию спринга

--- src/main/java/com/example/programengineeringautotests/ProgramEngineeringAutoTestsApplication.java
+++ src/main/java/com/example/programengineeringautotests/ProgramEngineeringAutoTestsApplication.java
@@ -8,7 +8,6 @@ public class ProgramEngineeringAutoTestsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProgramEngineeringAutoTestsApplication.class, args);
-//        ава
     }
 
 }
