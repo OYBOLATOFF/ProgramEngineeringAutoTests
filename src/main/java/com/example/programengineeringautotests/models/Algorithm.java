aeb88da8f4f3b94a59a78420210b481fbeacec7f
Oybolatov Ramazan <ramazanoybolatov@gmail.com>
2024-05-25 16:21:10 +0300

    Убрал говнокод

--- src/main/java/com/example/programengineeringautotests/models/Algorithm.java
+++ src/main/java/com/example/programengineeringautotests/models/Algorithm.java
@@ -4,7 +4,7 @@ import com.example.programengineeringautotests.exceptions.ArrayNotSortedExceptio
 
 public class Algorithm {
 
-    private static <T extends Comparable> void check_if_array_is_sorted(T[] array) throws ArrayNotSortedException {
+    private static <T extends Comparable> void checkIfArrayIsSorted(T[] array) throws ArrayNotSortedException {
         for (int i = 1; i < array.length; i += 1) {
             if (array[i].compareTo(array[i-1]) == -1) {
                 throw new ArrayNotSortedException();
@@ -13,17 +13,17 @@ public class Algorithm {
     }
 
     public static <T extends Comparable> int binarySearch(T[] array, T elem) throws ArrayNotSortedException {
-        check_if_array_is_sorted(array);
-        int l = 0; int r = array.length-1;
-        while (l < r) {
-            int midindex = (r + l) / 2;
-            T midelem = array[midindex];
-            if (midelem.compareTo(elem) == 1) {
-                l = midindex+1;
-            } else if (midelem.compareTo(elem) == -1) {
-                r = midindex-1;
+        checkIfArrayIsSorted(array);
+        int left = 0; int right = array.length-1;
+        while (left < right) {
+            int middleIndex = (right + left) / 2;
+            T middleElement = array[middleIndex];
+            if (middleElement.compareTo(elem) == 1) {
+                left = middleIndex+1;
+            } else if (middleElement.compareTo(elem) == -1) {
+                right = middleIndex-1;
             } else {
-                return midindex;
+                return middleIndex;
             }
         }
 
